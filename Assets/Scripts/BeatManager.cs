23a8f51bac7f9faefd847ce66505593353e7ccab
Max Gilbert <gilbertlmax@gmail.com>
2014-10-04 21:28:25 -0400

    Beat manager and beat event

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class BeatManager : MonoBehaviour {
 
+	private static BeatManager instance;
+	
+	public static BeatManager Instance
+	{
+		get 
+		{
+			return instance;
+		}
+	}
+	void Awake() {
+		instance = this;
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-	
+	public delegate void BeatEventHandler(BeatManager beatManager);
+	public event BeatEventHandler Beat;
+
+	public void OnBeat() {
+		if (Beat!=null) {
+			Beat(this);
+		}
+	}
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown("space")){
+			OnBeat();
+		}
 	}
+
+
 }
---
+++ Assets/Scripts/BeatPlatform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeatPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		BeatManager.Instance.Beat += BeatHandler;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void BeatHandler (BeatManager beatManeger) {
+		Debug.Log("Beat fired!");
+	}
+}
