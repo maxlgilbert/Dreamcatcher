796769c19578f9fe3774bdf801dafb539f5b690c
Judy Trinh <jtrinh74@gmail.com>
2014-10-07 20:53:32 -0400

    Added time elapsed UI element

---
+++ Assets/Scripts/MainUIManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainUIManager : MonoBehaviour {
+	public GUIText timeElapsed;
+
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		timeElapsed.text = Mathf.Round(Time.time).ToString();
+	}
+}
