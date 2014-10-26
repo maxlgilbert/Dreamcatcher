767013cb18287738a76c262b0ab46530e7de6e26
Judy Trinh <jtrinh74@gmail.com>
2014-10-26 02:00:20 -0400

    ANIMATIONS \o/\o/\o/ in JudysScene for now

---
+++ Assets/Scripts/SpriteAnimations.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimations : MonoBehaviour {
+
+	protected Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (animator) {
+
+			if (Input.GetKeyDown(KeyCode.UpArrow)) {
+				animator.SetTrigger("StartJumping");
+			}
+			if (Input.GetKeyUp(KeyCode.UpArrow)) {
+				animator.SetTrigger("EndJumping");
+			}
+			if (Input.GetKeyDown(KeyCode.RightArrow)) {
+				animator.SetBool("Running", true);
+			}
+			if (Input.GetKeyUp(KeyCode.RightArrow)) {
+				animator.SetBool("Running", false);
+			}
+		}
+
+	}
+}
